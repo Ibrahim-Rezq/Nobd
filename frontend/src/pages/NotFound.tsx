fe3d43867e99a3884c9036cca3deafe34da1be8c
Ibrahim-Rezq <ovic391@gmail.com.com>
2025-10-21 22:00:13 +0300

    chore: updated github mvp issue template and 404 page ui

--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -1,21 +1,26 @@
 import { useLocation } from 'react-router-dom'
 import { useEffect } from 'react'
+import { Button } from '@/components/ui/button'
+import { useNavigate } from 'react-router-dom'
+import { SquareArrowLeft } from 'lucide-react'
 
 const NotFound = () => {
 	const location = useLocation()
+	const navigate = useNavigate()
 
 	useEffect(() => {
 		console.error('404 Error: User attempted to access non-existent route:', location.pathname)
 	}, [location.pathname])
 
 	return (
-		<div className="flex min-h-screen items-center justify-center bg-gray-100">
+		<div className="flex min-h-[calc(100vh-8rem)] md:min-h-[calc(100vh-5rem)] items-center justify-center ">
 			<div className="text-center">
 				<h1 className="mb-4 text-4xl font-bold">404</h1>
 				<p className="mb-4 text-xl text-gray-600">Oops! Page not found</p>
-				<a href="/" className="text-blue-500 underline hover:text-blue-700">
+				<Button onClick={() => navigate('/')} variant="secondary" size="lg" className="rounded-2xl">
+					<SquareArrowLeft />
 					Return to Home
-				</a>
+				</Button>
 			</div>
 		</div>
 	)
